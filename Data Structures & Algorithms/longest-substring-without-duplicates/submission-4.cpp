2ebc42ff6698b1f9b310a030ce5e1d9a8e7af917
Junaid Hassan <123434178+JunaidHassan19@users.noreply.github.com>
2026-09-13 22:19:05 +0530

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> charSet;
+        int maxLength = 0;
+        int left = 0;
+        int n = s.size();
+
+        for (int right = 0; right < n; right++) {
+            while (charSet.find(s[right]) != charSet.end()) {
+                charSet.erase(s[left]);
+                left++;
+            }
+            charSet.insert(s[right]);
+            maxLength = max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+};
